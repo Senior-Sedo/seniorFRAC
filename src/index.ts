964eab75628b3deaebfde5e770558ca4412c5112
Senior Sedo <145229171+Senior-Sedo@users.noreply.github.com>
2024-11-27 17:03:36 +0100

    Minified /dist + Added JSDoc + CDN Support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,40 @@
+/**
+ * Generates a random number between min and max.
+ * @param {number} min - required
+ * @param {number} max - required
+ * @returns {number} number
+ * @throws TypeError if either min, max or fractionDigits is NaN
+ * @throws RangeError if the fractionDigits is less than 0
+ *
+ * @example
+ * import seniorFRAC from "@ssts/frac";
+ * const randomDecimal: number = seniorFRAC(-10, 10);
+ *
+ * console.log(randomDecimal);
+ * // Log example: 8.158
+ */
 const seniorFRAC = (min: number, max: number, fractionDigits: number = 3): number => {
-  const m: number = +("1" + "0".repeat(fractionDigits));
-  return Math.floor((Math.random() * (max - min) + min) * m) / m;
+  if (typeof min !== "number") throw new TypeError("min must be a number");
+  if (typeof max !== "number") throw new TypeError("max must be a number");
+  if (typeof fractionDigits !== "number") throw new TypeError("fractionDigits must be a number");
+  else if (fractionDigits < 0) throw new RangeError(`Invalid fractionDigits value: ${fractionDigits}. Must be greater than or equal to 0`);
+
+  const multiplier: number = parseInt("1" + "0".repeat(Math.floor(fractionDigits)));
+
+  return Math.floor((Math.random() * (max - min) + min) * multiplier) / multiplier;
 };
+
+declare global {
+  interface Window {
+    ssts: {
+      seniorFRAC?: typeof seniorFRAC;
+    };
+  }
+}
+
+if (window != undefined) {
+  window.ssts = {};
+  window.ssts.seniorFRAC = seniorFRAC;
+}
+
 export default seniorFRAC;
